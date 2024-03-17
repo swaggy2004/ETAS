448572ef3e85a6083331b7c490b8d953ec26a0a0
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-17 23:24:27 +0530

    Made it so that a new df will be made with the avaraged values

--- components/line_chart.py
+++ components/line_chart.py
@@ -6,9 +6,9 @@ engine = sqlalchemy.create_engine(
     'mysql+pymysql://python:python123!@localhost:3306/ETAS_IOT')
 
 current_date = datetime.now().date()
-sql = text("SELECT collectedDate, phValue, tdsValue, tempValue, turbidityValue FROM datalogs WHERE DATE(collectedDate) = :current_date")
+sql = f"SELECT collectedDate, phValue, tdsValue, tempValue, turbidityValue FROM datalogs WHERE DATE(collectedDate) = '{current_date}'"
 
-df = pd.read_sql(sql, engine, params={'current_date': current_date})
+df = pd.read_sql(sql, engine)
 
 hourly_avg = df.groupby(df['collectedDate'].dt.hour).mean()
 
