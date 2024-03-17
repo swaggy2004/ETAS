13e9678170e6786c7bc3b787af52f6a44942c08a
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-17 23:22:29 +0530

    Made it so that a new df will be made with the avaraged values

--- components/line_chart.py
+++ components/line_chart.py
@@ -6,15 +6,10 @@ engine = sqlalchemy.create_engine(
     'mysql+pymysql://python:python123!@localhost:3306/ETAS_IOT')
 
 current_date = datetime.now().date()
-# Select only the required columns from the database
-# Select only the required columns from the database
-sql = f"SELECT collectedDate, phValue, tdsValue, tempValue, turbidityValue FROM datalogs WHERE DATE(collectedDate) = '{
-    current_date}'"
+sql = text("SELECT collectedDate, phValue, tdsValue, tempValue, turbidityValue FROM datalogs WHERE DATE(collectedDate) = :current_date")
 
-# Read data from the database into a DataFrame
-df = pd.read_sql(sql, engine)
+df = pd.read_sql(sql, engine, params={'current_date': current_date})
 
-# Calculate hourly average values
 hourly_avg = df.groupby(df['collectedDate'].dt.hour).mean()
 
 # Extract hour from collectedDate and add it as a separate column
