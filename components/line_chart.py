ca4cefda5806c16664196257e18520dfb4fe7811
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-17 23:29:06 +0530

    Made it so that a new df will be made with the avaraged values

--- components/line_chart.py
+++ components/line_chart.py
@@ -6,8 +6,7 @@ engine = sqlalchemy.create_engine(
     'mysql+pymysql://python:python123!@localhost:3306/ETAS_IOT')
 
 current_date = datetime.now().date()
-sql = f"SELECT collectedDate, phValue, tdsValue, tempValue, turbidityValue FROM datalogs WHERE DATE(collectedDate) = '{
-    current_date}'"
+sql = f"SELECT collectedDate, phValue, tdsValue, tempValue, turbidityValue FROM datalogs WHERE DATE(collectedDate) = '{current_date}'"
 
 df = pd.read_sql(sql, engine)
 
