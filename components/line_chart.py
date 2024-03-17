73dc7d39fe5091b480b6d5241df3ea1f2a84c1ac
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-17 23:11:50 +0530

    Made it so that a new df will be made with the avaraged values

--- components/line_chart.py
+++ components/line_chart.py
@@ -13,4 +13,17 @@ sql = f"SELECT * FROM datalogs WHERE DATE(collectedDate) = '{current_date}'"
 
 
 df = pd.read_sql(sql, engine)
+
+# Calculate hourly average values
+hourly_avg = df.groupby(df['collectedDate'].dt.hour).mean()
+
+# Extract hour from collectedDate and add it as a separate column
+hourly_avg['hour'] = hourly_avg.index
+
+# Reorder columns as per requirement
+hourly_avg = hourly_avg[['phValue', 'tdsValue',
+                         'tempValue', 'turbidityValue', 'hour']]
+
 print(df)
+
+print(hourly_avg)
