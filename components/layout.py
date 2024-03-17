c133538f55c45860d1faacd4a313bd4ca00456e0
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-17 22:04:35 +0530

    Fixed the spinenr position

--- components/layout.py
+++ components/layout.py
@@ -1,16 +1,20 @@
 from dash import Dash, html
 import components.test as test
 import imports  # Import the imports module
-import components.frequency_tab as frequency_tab  # Import the frequency_tab module
+# Import the frequency_tab module
+import components.frequency_tab as frequency_tab
 # from components import line_chart  # Import the line_chart module
+
 
 def create_layout(app: Dash) -> imports.dbc.Container:
     return imports.dbc.Container(
         id="main-layout",
         children=[
             html.H1(app.title),
-            html.H2("Live Updates", className="text-center h2 fw-semibold"),
-            imports.dbc.Spinner(color="danger", type="grow", size="sm"),
+            imports.dcb.Row([
+                html.H2("Live Updates", className="text-center h2 fw-semibold"),
+                imports.dbc.Spinner(color="danger", type="grow", size="sm"),
+            ]),
             test.render(app),
             html.H2("Frequency of Data"),
             frequency_tab.render(app),
