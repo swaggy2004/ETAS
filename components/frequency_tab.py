00bd09689d56f4a3e3a50e9e37a0d29c7301f45e
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 22:26:09 +0530

    Checking the layout of the button group

--- components/frequency_tab.py
+++ components/frequency_tab.py
@@ -6,14 +6,14 @@ import dash_bootstrap_components as dbc
 
 def render(app: Dash) -> imports.dbc.Row:
     return imports.dbc.Row(
-        className="mb-5 text-center",
+        className="mb-5 text-center border border-2 border-dark",
         children=[
             imports.dbc.Col(
                 dcc.RadioItems(
                     id=ids.DATA_FREQUENCY,
-                    className="btn-group",
-                    inputClassName="btn-check",
-                    labelClassName="btn btn-primary fs-4",
+                    className="btn-group border border-2 border-success",
+                    inputClassName="btn-check border border-2 border-black",
+                    labelClassName="btn btn-primary fs-4 border border-2 border-danger",
                     options=[
                         {"label": "Daily", "value": "Daily"},
                         {"label": "Weekly", "value": "Weekly"},
@@ -22,7 +22,7 @@ def render(app: Dash) -> imports.dbc.Row:
                     value="Daily",
                     inline=True,
                 ),
-                className="radio-group",
+                className="radio-group border border-2 border-danger",
             ),
         ]
     )
