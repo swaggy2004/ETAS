ea2dff99feeb648ad919cd064b2d6d0aa8aebb2c
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 10:37:53 +0530

    Made the inital graphs

--- components/line_chart.py
+++ components/line_chart.py
@@ -18,33 +18,34 @@ df.drop(columns=['collectedDate'], inplace=True)
 hourly_avg = df.groupby('hour').mean()
 
 fig1 = px.line(hourly_avg, x=hourly_avg.index,
-              y=hourly_avg["phValue"], title="Hourly Average")
+               y=hourly_avg["phValue"], title="Hourly Average pH")
 
 fig2 = px.line(hourly_avg, x=hourly_avg.index,
-               y=hourly_avg["tdsValue"], title="Hourly Average")
+               y=hourly_avg["tdsValue"], title="Hourly Average TDS")
 
 fig3 = px.line(hourly_avg, x=hourly_avg.index,
-               y=hourly_avg["tempValue"], title="Hourly Average")
+               y=hourly_avg["tempValue"], title="Hourly Average Temperature")
 
 fig4 = px.line(hourly_avg, x=hourly_avg.index,
-               y=hourly_avg["turbidityValue"], title="Hourly Average")
+               y=hourly_avg["turbidityValue"], title="Hourly Average Turbidity")
+
 
 def render(app: Dash) -> dbc.Row:
     return dbc.Row([
         dcc.Graph(
-            id="line-chart",
+            id="line-chart-ph",
             figure=fig1
         ),
         dcc.Graph(
-            id="line-chart",
+            id="line-chart-tds",
             figure=fig2
         ),
         dcc.Graph(
-            id="line-chart",
+            id="line-chart-temp",
             figure=fig3
         ),
         dcc.Graph(
-            id="line-chart",
+            id="line-chart-turbidity",
             figure=fig4
         ),
     ],
