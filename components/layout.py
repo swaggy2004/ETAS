41599e0b3f10260af3aba652838b9533651e5cd7
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 19:39:08 +0530

    Checking the layout of the button group

--- components/layout.py
+++ components/layout.py
@@ -9,9 +9,9 @@ def create_layout(app: Dash) -> imports.dbc.Container:
         id="main-layout",
         children=[
             html.H1(app.title),
-            html.H2("Live Updates", className="text-center h2 fw-semibold"),
+            html.H2("Live Updates", className="text-center h2 fw-semibold mb-5"),
             live_update.render(app),
-            html.H2("Frequency of Data", className="text-center h2 fw-semibold mb-4"),
+            html.H2("Frequency of Data", className="text-center h2 fw-semibold mb-5"),
             frequency_tab.render(app),
             line_chart.render(app),
         ],
