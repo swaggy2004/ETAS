b4a294ff5f855bfdc510ad71c3fb99865dea0d42
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 10:42:52 +0530

    Made the inital graphs

--- components/line_chart.py
+++ components/line_chart.py
@@ -12,36 +12,41 @@ current_date = datetime.now().date()
 sql = f"SELECT collectedDate, phValue, tdsValue, tempValue, turbidityValue FROM datalogs WHERE DATE(collectedDate) = '{current_date}'"
 
 df = pd.read_sql(sql, engine)
+
 df['collectedDate'] = pd.to_datetime(df['collectedDate'])
 df['hour'] = df['collectedDate'].dt.hour
 df.drop(columns=['collectedDate'], inplace=True)
 hourly_avg = df.groupby('hour').mean()
 
-fig1 = px.line(hourly_avg, x=hourly_avg.index, y=hourly_avg["phValue"], title="Hourly Average pH")
-
-fig2 = px.line(hourly_avg, x=hourly_avg.index, y=hourly_avg["tdsValue"], title="Hourly Average TDS")
-
-fig3 = px.line(hourly_avg, x=hourly_avg.index,y=hourly_avg["tempValue"], title="Hourly Average Temperature")
-
-fig4 = px.line(hourly_avg, x=hourly_avg.index,y=hourly_avg["turbidityValue"], title="Hourly Average Turbidity")
+print(hourly_avg.head())  # Print the first few rows of the DataFrame
+print(hourly_avg.dtypes)  # Print the data types of each column
 
 
-def render(app: Dash) -> dbc.Row:
-    return dbc.Row([
-        dcc.Graph(
-            id="line-chart-ph",
-            figure=fig1
-        ),
-        dcc.Graph(
-            id="line-chart-tds",
-            figure=fig2
-        ),
-        dcc.Graph(
-            id="line-chart-temp",
-            figure=fig3
-        ),
-        dcc.Graph(
-            id="line-chart-turbidity",
-            figure=fig4
-        ),
-    ],)
+# fig1 = px.line(hourly_avg, x=hourly_avg.index, y=hourly_avg["phValue"], title="Hourly Average pH")
+
+# fig2 = px.line(hourly_avg, x=hourly_avg.index, y=hourly_avg["tdsValue"], title="Hourly Average TDS")
+
+# fig3 = px.line(hourly_avg, x=hourly_avg.index,y=hourly_avg["tempValue"], title="Hourly Average Temperature")
+
+# fig4 = px.line(hourly_avg, x=hourly_avg.index,y=hourly_avg["turbidityValue"], title="Hourly Average Turbidity")
+
+
+# def render(app: Dash) -> dbc.Row:
+#     return dbc.Row([
+#         dcc.Graph(
+#             id="line-chart-ph",
+#             figure=fig1
+#         ),
+#         dcc.Graph(
+#             id="line-chart-tds",
+#             figure=fig2
+#         ),
+#         dcc.Graph(
+#             id="line-chart-temp",
+#             figure=fig3
+#         ),
+#         dcc.Graph(
+#             id="line-chart-turbidity",
+#             figure=fig4
+#         ),
+#     ],)
