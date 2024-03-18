d219285b506446c1190c97aed82d2c515f128a0c
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 23:15:59 +0530

    Layout updates

--- components/live_update.py
+++ components/live_update.py
@@ -75,15 +75,12 @@ def render(app: Dash) -> imports.dbc.Row:
         children=[
             imports.dbc.Col(
                 id="pH",
-                className="mb-3"
             ),
             imports.dbc.Col(
                 id="temp",
-                className="mb-3"
             ),
             imports.dbc.Col(
                 id="tds",
-                className="mb-3"
             ),
             imports.dbc.Col(
                 id="turbidity",
@@ -94,5 +91,5 @@ def render(app: Dash) -> imports.dbc.Row:
                 n_intervals=0
             ),
         ],
-        className="justify-content-center align-items-center row-cols-1 row-cols-md-2 row-cols-lg-4 mb-5 text-center p-0"
+        className="justify-content-center align-items-center row-cols-1 row-cols-md-2 row-cols-lg-4 mb-5 text-center p-0 gap-3"
     )
