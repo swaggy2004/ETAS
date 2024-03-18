0fe8a863b8071629fd79e96888e93eb4afa6d657
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 19:44:37 +0530

    Layout updates

--- components/frequency_tab.py
+++ components/frequency_tab.py
@@ -11,7 +11,7 @@ def render(app: Dash) -> imports.dbc.Row:
             dcc.RadioItems(
                 id=ids.DATA_FREQUENCY,
                 className="btn-group",
-                inputClassName="btn-check",
+                inputClassName="btn-check w-100",
                 labelClassName="btn btn-primary w-100",
                 options=[
                     {"label": "Daily", "value": "Daily"},
@@ -22,48 +22,3 @@ def render(app: Dash) -> imports.dbc.Row:
             ),
         ]
     )
-
-# def render1(app: Dash) -> imports.dbc.Row:
-#     return imports.dbc.Row([
-#         dbc.ButtonGroup(
-#             [
-#                 dbc.Button(
-#                     "Daily",
-#                     id=ids.DATA_FREQUENCY + "-daily",
-#                     n_clicks=0,
-#                     class_name="w-100 fs-5",
-#                 ),
-#                 dbc.Button(
-#                     "Weekly",
-#                     id=ids.DATA_FREQUENCY + "-weekly",
-#                     n_clicks=0,
-#                     class_name="w-100 fs-5",
-#                 ),
-#                 dbc.Button(
-#                     "Monthly",
-#                     id=ids.DATA_FREQUENCY + "-monthly",
-#                     n_clicks=0,
-#                     class_name="w-100 fs-5",
-#                 ),
-#             ],
-#             id=ids.DATA_FREQUENCY,
-#             className="p-0",
-#         )
-#     ],
-#         className="justify-content-center align-items-center row-cols-1 row-cols-md-3 row-cols-lg-3 w-100"
-#     )
-
-# def render(app: Dash) -> imports.dbc.Row:
-#     frequency = ["Daily", "Weekly", "Monthly"]
-#     return imports.dbc.Row(
-#         children=[
-#             dcc.RadioItems(
-#                 id=ids.DATA_FREQUENCY,
-#                 options=[
-#                     {"label": fq, "value": fq} for fq in frequency,
-#                 ],
-#                 value="Daily",
-#                 className="btn-group btn-group-lg",
-#             )
-#         ]
-#     )
