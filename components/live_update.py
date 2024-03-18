9a115e156254c54da248411b53f9c2c20428ab4c
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 23:18:04 +0530

    Layout updates on live_updates.py

--- components/live_update.py
+++ components/live_update.py
@@ -91,5 +91,5 @@ def render(app: Dash) -> imports.dbc.Row:
                 n_intervals=0
             ),
         ],
-        className="justify-content-center align-items-center row-cols-1 row-cols-md-2 row-cols-lg-4 mb-5 text-center p-0 gap-4"
+        className="justify-content-center align-items-center row-cols-1 row-cols-md-2 row-cols-lg-4 mb-5 text-center"
     )
