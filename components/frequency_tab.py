ab147dca5f5a9587bc1e2cb2fb577effc3e70502
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 21:36:29 +0530

    Checking the layout of the button group

--- components/frequency_tab.py
+++ components/frequency_tab.py
@@ -6,11 +6,11 @@ import dash_bootstrap_components as dbc
 
 def render(app: Dash) -> imports.dbc.Row:
     return imports.dbc.Row(
-        className="",
+        className="mb-5",
         children=[
             dcc.RadioItems(
                 id=ids.DATA_FREQUENCY,
-                className="btn-group",
+                className="btn-group w-75",
                 inputClassName="btn-check",
                 labelClassName="btn btn-primary fs-5 fs-md-4",
                 options=[
