6ecebee3dc2f89aee0f0b44616d91a3e849e992e
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 23:04:27 +0530

    Layout updates

--- components/live_update.py
+++ components/live_update.py
@@ -75,19 +75,19 @@ def render(app: Dash) -> imports.dbc.Row:
         children=[
             imports.dbc.Col(
                 id="pH",
-                class_name="p-4 mb-3 mb-md-0"
+                class_name="p-1 mb-3 mb-md-0"
             ),
             imports.dbc.Col(
                 id="temp",
-                class_name="p-4 mb-3 mb-md-0"
+                class_name="p-1 mb-3 mb-md-0"
             ),
             imports.dbc.Col(
                 id="tds",
-                class_name="p-4 mb-3 mb-md-0"
+                class_name="p-1 mb-3 mb-md-0"
             ),
             imports.dbc.Col(
                 id="turbidity",
-                class_name="p-4"
+                class_name="p-1"
             ),
             dcc.Interval(
                 id='interval-component',
