1616969d49c30209f3d607aa37fc56f0d0317b9e
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 22:46:33 +0530

    Layout updates

--- components/frequency_tab.py
+++ components/frequency_tab.py
@@ -13,7 +13,7 @@ def render(app: Dash) -> imports.dbc.Row:
                     id=ids.DATA_FREQUENCY,
                     className="btn-group btn-group-md",
                     inputClassName="btn-check",
-                    labelClassName="btn btn-primary fs-3",
+                    labelClassName="btn btn-primary fs-4",
                     options=[
                         {"label": "Daily", "value": "Daily"},
                         {"label": "Weekly", "value": "Weekly"},
