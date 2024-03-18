7ff6cf8e4908be10e0ee6430b335c4ebdb76c03e
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 21:27:16 +0530

    Layout updates

--- components/line_chart.py
+++ components/line_chart.py
@@ -8,24 +8,29 @@ from dash.dependencies import Output, Input
 from . import ids
 import numpy as np
 
+
 def get_data(val):
-    engine = sqlalchemy.create_engine('mysql+pymysql://python:python123!@localhost:3306/ETAS_IOT')
+    engine = sqlalchemy.create_engine(
+        'mysql+pymysql://python:python123!@localhost:3306/ETAS_IOT')
 
     if val == "Daily":
         current_date = datetime.now().date() - timedelta(days=1)
-        sql = f"SELECT collectedDate, phValue, tdsValue, tempValue, turbidityValue FROM datalogs WHERE DATE(collectedDate) = '{current_date}'"
+        sql = f"SELECT collectedDate, phValue, tdsValue, tempValue, turbidityValue FROM datalogs WHERE DATE(collectedDate) = '{
+            current_date}'"
 
     elif val == "Weekly":
         today = datetime.now().date()
         start_of_previous_week = today - timedelta(days=today.weekday() + 7)
         end_of_previous_week = start_of_previous_week + timedelta(days=6)
-        sql = f"SELECT collectedDate, phValue, tdsValue, tempValue, turbidityValue FROM datalogs WHERE DATE(collectedDate) BETWEEN '{start_of_previous_week}' AND '{end_of_previous_week}'"
+        sql = f"SELECT collectedDate, phValue, tdsValue, tempValue, turbidityValue FROM datalogs WHERE DATE(collectedDate) BETWEEN '{
+            start_of_previous_week}' AND '{end_of_previous_week}'"
 
     elif val == "Monthly":
         current_date = datetime.now().date().replace(day=1)
         next_month = current_date.replace(month=current_date.month+1)
         end_of_month = next_month - timedelta(days=1)
-        sql = f"SELECT collectedDate, phValue, tdsValue, tempValue, turbidityValue FROM datalogs WHERE DATE(collectedDate) BETWEEN '{current_date}' AND '{end_of_month}'"
+        sql = f"SELECT collectedDate, phValue, tdsValue, tempValue, turbidityValue FROM datalogs WHERE DATE(collectedDate) BETWEEN '{
+            current_date}' AND '{end_of_month}'"
 
     else:
         return None
@@ -55,18 +60,18 @@ def process_data(df, frequency):
 
     elif frequency == "Weekly":
         # Convert 'collectedDate' to day of the week and specify desired order
-        new_df['day_of_week'] = pd.Categorical(new_df['collectedDate'].dt.day_name(),categories=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],ordered=True)
+        new_df['day_of_week'] = pd.Categorical(new_df['collectedDate'].dt.day_name(), categories=[
+                                               "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], ordered=True)
 
         # Group by 'day_of_week' and calculate the mean
         weekly_avg = new_df.groupby('day_of_week').mean()
 
         # Reindex to include all days of the week and fill missing values with NaN
-        weekly_avg = weekly_avg.reindex(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], fill_value=np.nan)
-        
+        weekly_avg = weekly_avg.reindex(
+            ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], fill_value=np.nan)
+
         print(weekly_avg)
         return weekly_avg
-
-
 
     elif frequency == "Monthly":
         # Set 'collectedDate' as index and drop other columns
@@ -122,4 +127,4 @@ def render(app: Dash) -> dbc.Row:
         dcc.Graph(id="line-chart-tds"),
         dcc.Graph(id="line-chart-temp"),
         dcc.Graph(id="line-chart-turbidity")
-    ])
+    ], className="border border-2 border-danger")
