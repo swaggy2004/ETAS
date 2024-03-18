63b08325ce6e75bee8368d6fc2446d9d7cc5d268
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 20:12:10 +0530

    Layout updates

--- components/layout.py
+++ components/layout.py
@@ -1,18 +1,20 @@
 from dash import Dash, html
 import components.live_update as live_update
-import imports  
+import imports
 import components.frequency_tab as frequency_tab
-from components import line_chart  
+from components import line_chart
+
 
 def create_layout(app: Dash) -> imports.dbc.Container:
     return imports.dbc.Container(
         id="main-layout",
         children=[
             html.H1(app.title),
-            html.H2("Live Updates", className="text-center h2 fw-semibold mb-3 border border-3 border-danger"),
+            imports.dbc.Row(html.H2(
+                "Live Updates", className="text-center h2 fw-semibold mb-3 border border-3 border-danger")),
             live_update.render(app),
-            html.H2("Frequency of Data",
-                    className="text-center h2 fw-semibold mb-3 border border-3 border-danger"),
+            imports.dbc.Row(html.H2("Frequency of Data",
+                                    className="text-center h2 fw-semibold mb-3 border border-3 border-danger")),
             frequency_tab.render(app),
             line_chart.render(app),
         ],
