af8b4a891c516ddc244ddd5081e3d2dbcd98f5a1
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 20:47:43 +0530

    Layout updates

--- components/frequency_tab.py
+++ components/frequency_tab.py
@@ -6,11 +6,11 @@ import dash_bootstrap_components as dbc
 
 def render(app: Dash) -> imports.dbc.Row:
     return imports.dbc.Row(
-        className="mb-5 px-0 px-md-2 justify-content-center align-items-center",
+        className="mb-5 justify-content-center align-items-center",
         children=[
             dcc.RadioItems(
                 id=ids.DATA_FREQUENCY,
-                className="btn-group p-0 w-md-75 w-100",
+                className="btn-group p-2 w-100",
                 inputClassName="btn-check",
                 labelClassName="btn btn-primary fs-5 fs-md-4",
                 options=[
