142c63db903311fa8373778a1972b98d108097fb
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 21:52:23 +0530

    Layout updates

--- components/frequency_tab.py
+++ components/frequency_tab.py
@@ -11,7 +11,7 @@ def render(app: Dash) -> imports.dbc.Row:
             imports.dbc.Col(
                 dcc.RadioItems(
                     id=ids.DATA_FREQUENCY,
-                    className="btn-group btn-group-lg",
+                    className="btn-group btn-group-lg border border-2 border-dark",
                     inputClassName="btn-check",
                     labelClassName="btn btn-primary fs-4 fs-md-4",
                     options=[
@@ -22,7 +22,7 @@ def render(app: Dash) -> imports.dbc.Row:
                     value="Daily",
                     inline=True,
                 ),
-                class_name="justify-content-center align-items-center"
+                class_name="justify-content-center align-items-center border border-2 border-danger"
             )
         ]
     )
