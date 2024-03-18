6e1eed024987b3b56e783e6009f2e01dfc074bd0
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 19:53:28 +0530

    Layout updates

--- components/frequency_tab.py
+++ components/frequency_tab.py
@@ -11,7 +11,7 @@ def render(app: Dash) -> imports.dbc.Row:
             dcc.RadioItems(
                 id=ids.DATA_FREQUENCY,
                 className="btn-group w-100 p-0",
-                inputClassName="btn-check w-100 p-0",
+                inputClassName="btn-check w-100",
                 labelClassName="btn btn-primary w-100 fs-4 border border-3 border-danger",
                 options=[
                     {"label": "Daily", "value": "Daily"},
