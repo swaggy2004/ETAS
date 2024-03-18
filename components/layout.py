a19e7b7196d11a494dded1f30e9414829ce0c0ec
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-18 18:31:17 +0530

    Checking the layout of the button group

--- components/layout.py
+++ components/layout.py
@@ -13,7 +13,6 @@ def create_layout(app: Dash) -> imports.dbc.Container:
             live_update.render(app),
             html.H2("Frequency of Data"),
             frequency_tab.render(app),
-            frequency_tab.render1(app),
             line_chart.render(app),
         ],
     )
