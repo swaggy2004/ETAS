8ecdcef854dbd400508c14094c9b0c09cb7b274d
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-21 14:26:13 +0530

    Bug fix: I fixed the imports for the bootstrap

--- components/mapview.py
+++ components/mapview.py
@@ -1,11 +1,12 @@
-import dash as Dash
+from dash import Dash, html
 import dash_bootstrap_components as dbc
+import imports
 import plotly.express as px
 
 fig = px.density_mapbox(center=dict(lat=6.820854124676803, lon=80.03954479062239), zoom=18,
                         mapbox_style="open-street-map")
 
-def render_map(app:Dash) -> dbc.Row:
-    return dbc.Row(
+def render_map(app:Dash) -> imports.dbc.Row:
+    return imports.dbc.Row(
         children=fig.show()
     )
