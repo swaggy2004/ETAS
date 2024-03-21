c4ac6eb92f468da459872cfd11ef4453070b2991
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-21 16:40:11 +0530

    Made the button to turn on off the motor

--- components/motor_button.py
+++ components/motor_button.py
@@ -4,10 +4,9 @@ import dash_bootstrap_components as dbc
 
 def render(app: Dash) -> dbc.Row:
     return dbc.Row([
-        dbc.Checklist(
-            options=[{"Pump": "On", "value": 0},],
-            value=0,
-            id="motor-button",
-            switch=True
+        dbc.Switch(
+            id="motor-switch",
+            label="Motor",
+            value=False,
         )
     ])
