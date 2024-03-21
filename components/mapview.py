7aabdc4723364a0646556a7852481f8851ac24c7
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-21 14:40:34 +0530

    Correctly added the import

--- components/mapview.py
+++ components/mapview.py
@@ -1,6 +1,5 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
 import imports
 import plotly.express as px
 
