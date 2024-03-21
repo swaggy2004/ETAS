3d3b0d6d68e3c726d62597aa6c10b5fd289924dc
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-21 16:15:24 +0530

    Added a margin from the top for the main title

--- components/layout.py
+++ components/layout.py
@@ -10,7 +10,7 @@ def create_layout(app: Dash) -> imports.dbc.Container:
     return imports.dbc.Container(
         id="main-layout",
         children=[
-            html.H1(app.title, className="display-1 text-center mb-4"),
+            html.H1(app.title, className="display-1 text-center mb-4 mt-3"),
             html.H2("Live Updates", className="text-center h2 fw-lighter mb-3"),
             live_update.render(app),
             html.H2("Frequency of Data",
