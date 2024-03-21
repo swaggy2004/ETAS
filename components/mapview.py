c903fbc64f2338317603d013438906f254048f95
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-21 14:39:01 +0530

    Added the dash core componenets library to mapview.py

--- components/mapview.py
+++ components/mapview.py
@@ -1,5 +1,6 @@
 from dash import Dash, html
 import dash_bootstrap_components as dbc
+import dash_core_components as dcc
 import imports
 import plotly.express as px
 
@@ -7,6 +8,6 @@ fig = px.density_mapbox(center=dict(lat=6.820854124676803, lon=80.03954479062239
                         mapbox_style="open-street-map")
 
 def render_map(app:Dash) -> imports.dbc.Row:
-    return imports.dbc.Row(
-        children=fig.show()
-    )
+    return imports.dbc.Row([
+        dcc.Graph(figure=fig)
+    ])
