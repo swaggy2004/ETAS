ed68d1ca75b4ad9b671bb7e81f98540e9706c5a1
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-31 13:50:40 +0530

    Made the labels for the toggle switch

--- components/motor_button.py
+++ components/motor_button.py
@@ -1,20 +1,34 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 
 
 def render(app: Dash) -> dbc.Row:
+    @app.callback(
+        Output("motor-switch-label", "children"),
+        Input("motor-switch", "value"),
+    )
+    def toggle_label(switch_value):
+        if switch_value:
+            return "ON"
+        else:
+            return "OFF"
+
     return dbc.Row(
         [
             dbc.Col(
-                dbc.Switch(
-                    id="motor-switch",
-                    label="On",
-                    value=False,
-                    className="mx-auto"
-                ),
+                [
+                    dbc.Label(id="motor-switch-label"),
+                    dbc.Switch(
+                        id="motor-switch",
+                        value=False,
+                        className="mx-auto",
+                        labelClassName="switch-label",
+                        labelPosition="start" if not switch_value else "end",
+                    ),
+                ],
                 width="auto",
-                className="d-flex justify-content-center align-items-center"
+                className="d-flex justify-content-center align-items-center",
             )
         ],
-        className="justify-content-center align-items-center fs-1"
+        className="justify-content-center align-items-center fs-1",
     )
