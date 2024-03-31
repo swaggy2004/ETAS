242a96a85fb96656da1cc21922c3e601f319dad8
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-31 13:44:15 +0530

    Toggle button fixed

--- components/motor_button.py
+++ components/motor_button.py
@@ -3,16 +3,18 @@ import dash_bootstrap_components as dbc
 
 
 def render(app: Dash) -> dbc.Row:
-    return dbc.Row([
-        dbc.Col(
-            
-        ),
-        dbc.Switch(
-            id="motor-switch",
-            label="On",
-            value=False,
-            className="justify-content-center align-items-center fs-1"
-        ),
-    ],
+    return dbc.Row(
+        [
+            dbc.Col(
+                dbc.Switch(
+                    id="motor-switch",
+                    label="On",
+                    value=False,
+                    className="mx-auto"
+                ),
+                width="auto",
+                className="d-flex justify-content-center align-items-center"
+            )
+        ],
         className="justify-content-center align-items-center fs-1"
     )
