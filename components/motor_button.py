c856bd40f1836a75d7028895f7fbb3c66b95865d
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-31 13:40:57 +0530

    Centering the toggle button

--- components/motor_button.py
+++ components/motor_button.py
@@ -4,10 +4,14 @@ import dash_bootstrap_components as dbc
 
 def render(app: Dash) -> dbc.Row:
     return dbc.Row([
+        dbc.Col(
+            
+        ),
         dbc.Switch(
             id="motor-switch",
             label="On",
             value=False,
+            className="justify-content-center align-items-center fs-1"
         ),
     ],
         className="justify-content-center align-items-center fs-1"
