097966f88209c4f0517b5df93865f1d244916ea5
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-31 15:23:55 +0530

    New map view

--- components/mapview.py
+++ components/mapview.py
@@ -2,8 +2,12 @@ from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 import plotly.express as px
 
+token = "e84ee379-3451-4256-a488-7987b44674fd"
+
 fig = px.density_mapbox(center=dict(lat=6.820854124676803, lon=80.03954479062239), zoom=18,
                         mapbox_style="stamen-terrain")
+
+fig.update_layout(mapbox_accesstoken=token)
 
 def render_map(app:Dash) -> dbc.Row:
     return dbc.Row([
