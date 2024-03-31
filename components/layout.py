964032ecd6481c61c87d68df9261b060766dade9
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-31 19:28:58 +0530

    Layout fix

--- components/layout.py
+++ components/layout.py
@@ -12,7 +12,8 @@ def create_layout(app: Dash) -> imports.dbc.Container:
         id="main-layout",
         children=[
             html.H1(app.title, className="display-1 text-center mb-1 mt-5"),
-            html.H4("Enviro Track Aqua Shield", className="text-center fw-lighter mb-4"),
+            html.H4("(Enviro Track Aqua Shield)",
+                    className="text-center fw-lighter mb-5"),
             html.H2("Motor Control", className="text-center h2 fw-lighter mb-3"),
             motor_button.render(app),
             html.H2("Live Updates", className="text-center h2 fw-lighter mb-3"),
@@ -21,7 +22,7 @@ def create_layout(app: Dash) -> imports.dbc.Container:
                     className="text-center h2 fw-lighter mb-3"),
             frequency_tab.render(app),
             line_chart.render(app),
-            html.H2("Purity Map View", className="text-center h2 fw-lighter mb-2"),
+            html.H2("Purity Map View", className="text-center h2 fw-lighter"),
             mapview.render_map(app),
         ],
     )
