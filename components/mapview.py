5ed97f71b4474df5800ce12f489e836c68a74a61
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-31 18:05:08 +0530

    Changed the map layout

--- components/mapview.py
+++ components/mapview.py
@@ -1,7 +1,5 @@
 import sqlalchemy
 import pandas as pd
-from dash import Dash, html, dcc
-import dash_bootstrap_components as dbc
 import plotly.express as px
 
 
@@ -10,7 +8,7 @@ def get_data():
         'mysql+pymysql://python:python123!@localhost:3306/ETAS_IOT')
 
     # SQL query to fetch all records
-    sql = "SELECT collectedDate, longitude, latitude, phValue, tdsValue, tempValue, turbidityValue FROM datalogs"
+    sql = "SELECT collectedDate, latitude, longitude, phValue, tdsValue, tempValue, turbidityValue FROM datalogs"
 
     # Read data from SQL into DataFrame
     df = pd.read_sql(sql, engine)
@@ -37,33 +35,34 @@ def calculate_purity_index(df, weights={'ph': 0.25, 'turbidity': 0.25, 'temp': 0
     return df
 
 
-def render_map(app: Dash) -> dbc.Row:
+def render_heatmap():
     # Fetch data
     data = get_data()
 
     # Drop rows with non-numeric latitude or longitude values
-    data = data.dropna(subset=['latitude', 'longitude'], how='any')
+    data = data.dropna(
+        subset=['latitude', 'longitude', 'purity_index'], how='any')
     data['latitude'] = pd.to_numeric(data['latitude'], errors='coerce')
     data['longitude'] = pd.to_numeric(data['longitude'], errors='coerce')
 
     # Calculate purity index
     data_with_purity = calculate_purity_index(data)
 
-    # Plot map
-    fig = px.scatter_mapbox(data_with_purity,
-                            lat="latitude",
-                            lon="longitude",
-                            color="purity_index",
+    # Plot heatmap
+    fig = px.density_mapbox(data_with_purity,
+                            lat='latitude',
+                            lon='longitude',
+                            z='purity_index',
+                            radius=10,
+                            center=dict(lat=0, lon=180),
+                            zoom=0,
+                            mapbox_style="open-street-map",
                             color_continuous_scale=px.colors.sequential.Viridis,
-                            size_max=15,
-                            zoom=10,
-                            hover_name="collectedDate",
-                            hover_data={"purity_index": True, "latitude": False, "longitude": False})
+                            opacity=0.6,
+                            title="Water Purity Heatmap")
 
-    fig.update_layout(mapbox_style="open-street-map")
-    fig.update_layout(title="Water Purity Map")
+    return fig.show()
 
-    # Return the map as a Dash component
-    return dbc.Row([
-        dcc.Graph(figure=fig)
-    ])
+
+# Example usage:
+render_heatmap()
