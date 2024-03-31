3aad58768231e8ebc29aa338eb2d6705eb1e6c97
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-03-31 14:48:36 +0530

    Changed the type of the map

--- components/mapview.py
+++ components/mapview.py
@@ -3,7 +3,7 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 
 fig = px.density_mapbox(center=dict(lat=6.820854124676803, lon=80.03954479062239), zoom=18,
-                        mapbox_style="open-street-map")
+                        mapbox_style="stamen-terrain")
 
 def render_map(app:Dash) -> dbc.Row:
     return dbc.Row([
