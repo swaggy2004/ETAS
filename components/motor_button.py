0cca9d0312f350995b48ce296c9d11f60359b6db
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-04-01 15:27:09 +0530

    Converted back to the original coe

--- components/motor_button.py
+++ components/motor_button.py
@@ -1,59 +1,25 @@
 from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State, MATCH
-import sqlalchemy
-import pandas as pd
-
-engine = sqlalchemy.create_engine(
-    'mysql+pymysql://python:python123!@localhost:3306/ETAS_IOT')
-
-
-def fetch_latest_motor_state():
-    try:
-        sql = "SELECT motorState FROM datalogs ORDER BY collectedDate DESC LIMIT 1"
-        with engine.connect() as connection:
-            result = connection.execute(sql)
-            row = result.fetchone()
-            if row:
-                motor_state = row[0]
-            else:
-                motor_state = None
-            print(motor_state)
-        return bool(motor_state)
-    except Exception as e:
-        print("Error fetching latest motor state:", e)
-        return None
 
 
 def render(app: Dash) -> dbc.Row:
     @app.callback(
-        Output("motor-switch", "value"),
-        Input("interval-component", "n_intervals")
+        Output("motor-switch", "label"),
+        Input("motor-switch", "value"),
     )
-    def update_motor_switch(n_intervals: int):
-        latest_motor_state = fetch_latest_motor_state()
-        if latest_motor_state is None:
-            return False
-        elif latest_motor_state == 1:
-            return True
-        else:
-            return 
-
+    def update_motor_switch_label(value: bool) -> str:
+        return "ON" if value else "OFF"
     return dbc.Row(
         [
             dbc.Col(
                 dbc.Switch(
                     id="motor-switch",
+                    label="On",
                     value=False,
-                    className="mx-auto"
+                    className="mx-auto"  # Add this line
                 ),
-                width="auto",
-                className="d-flex justify-content-center align-items-center"
-            ),
-            dcc.Interval(
-                id='interval-component',
-                interval=1 * 1000,  # in milliseconds
-                n_intervals=0
+                width="auto",  # Add this line
+                className="d-flex justify-content-center align-items-center"  # Add this line
             )
         ],
         className="justify-content-center align-items-center fs-1 mb-3"
