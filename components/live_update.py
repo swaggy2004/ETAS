62f83d05857154e2df70a0679ca761f1f02f2d75
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-04-01 16:03:39 +0530

    Fixed the live updates part

--- components/live_update.py
+++ components/live_update.py
@@ -1,56 +1,95 @@
-from dash import Dash, html, dcc, Input, Output
-import dash_bootstrap_components as dbc
+from dash import Dash, html, dcc
+import imports
+from dash.dependencies import Output, Input
 import sqlalchemy
 import pandas as pd
 
-engine = sqlalchemy.create_engine('mysql+pymysql://python:python123!@localhost:3306/ETAS_IOT')
+engine = sqlalchemy.create_engine(
+    'mysql+pymysql://python:python123!@localhost:3306/ETAS_IOT')
+
 
 def fetch_latest_data():
     try:
         # Construct SQL query to select the latest record from the database
         sql = "SELECT * FROM datalogs ORDER BY collectedDate DESC LIMIT 1"
+
         # Execute the SQL query and load result into a DataFrame
         df = pd.read_sql(sql, engine)
-        print(df);
+
         return df
+
     except Exception as e:
         print("Error fetching latest data:", e)
         return None
 
-def render(app: Dash) -> dbc.Row:
-    @app.callback(
-        Output("motor-switch", "value"),
-        Input("motor-switch", "n_clicks"),
-    )
-    def update_motor_switch_value(n_clicks: int) -> bool:
-        # Fetch the latest data from the database
-        latest_data = fetch_latest_data()
 
-        # If there is data available, check the motorState value
-        if not latest_data.empty:
-            motor_state = latest_data['motorState'].iloc[0]
-            if motor_state == 1:
-                # If motorState is 1, set the switch to True (ON)
-                return True
-            else:
-                # If motorState is 0, set the switch to False (OFF)
-                return False
-        else:
-            # If no data is available, set the switch to False (OFF) as a default
-            return False
-
-    return dbc.Row(
-        [
-            dbc.Col(
-                dbc.Switch(
-                    id="motor-switch",
-                    label="On",
-                    value=False,
-                    className="mx-auto",
+def make_card(card_title, card_value):
+    card = imports.dbc.Card(
+        imports.dbc.CardBody(
+            [
+                html.H5(card_title, className="card-title fs-6 text-center"),
+                html.H1(
+                    card_value,
+                    className="card-text h2 fw-sem-bold text-center",
                 ),
-                width="auto",
-                className="d-flex justify-content-center align-items-center",
-            )
+            ]
+        ),
+        className="w-100"
+    )
+    return card
+
+
+def render(app: Dash) -> imports.dbc.Row:
+    @app.callback(
+        Output("pH", "children"),
+        Output("temp", "children"),
+        Output("tds", "children"),
+        Output("turbidity", "children"),
+        Input("interval-component", "n_intervals"),
+    )
+    def update_metrics(n):
+        # Fetch latest data
+        df = fetch_latest_data()
+
+        if df is not None and not df.empty:
+            # Extract the relevant values from the DataFrame
+            latest_value_ph = df.iloc[0]['phValue']
+            latest_value_temp = df.iloc[0]['tempValue']
+            latest_value_turbidity = df.iloc[0]['turbidityValue']
+            latest_value_tds = df.iloc[0]['tdsValue']
+
+            # Create updated card components
+            ph_card = make_card("pH", f"{latest_value_ph}")
+            temp_card = make_card("Temperature", f"{latest_value_temp}°C")
+            turbidity_card = make_card(
+                "Turbidity", f"{latest_value_turbidity} %")
+            tds_card = make_card("Total Dissolved Solids",
+                                 f"{latest_value_tds} ppm")
+
+            return ph_card, temp_card, tds_card, turbidity_card
+        else:
+            return "No data available"
+
+    return imports.dbc.Row(
+        id="live-updates",
+        children=[
+            imports.dbc.Col(
+                id="pH",
+            ),
+            imports.dbc.Col(
+                id="temp",
+            ),
+            imports.dbc.Col(
+                id="tds",
+            ),
+            imports.dbc.Col(
+                id="turbidity",
+            ),
+            dcc.Interval(
+                id='interval-component',
+                interval=3*1000,  # Update every 1 second
+                n_intervals=0
+            ),
         ],
-        className="justify-content-center align-items-center fs-1 mb-3",
+        className="justify-content-center align-items-center row-cols-1 row-cols-md-2 row-cols-lg-4 mb-5 text-center gap-3 gap-lg-0"
     )
