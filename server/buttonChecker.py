adc21119c6010891178f0e9b2ca0d965663b6619
[Shaveen Balasooriya] <shaveenb2004@outlook.com>
2024-04-02 13:12:44 +0530

    Made a checker so that the button state is constent all thourgh out

---
+++ server/buttonChecker.py
@@ -0,0 +1,22 @@
+import sqlalchemy
+import pandas as pd
+
+engine = sqlalchemy.create_engine(
+    'mysql+pymysql://python:python123!@localhost:3306/ETAS_IOT')
+
+def fetch_motor_state(newState):
+    try:
+        # Construct SQL query to select the latest record from the database
+        sql = "SELECT motorState FROM datalogs ORDER BY collectedDate DESC LIMIT 1 OFFSET 1;"
+
+        # Execute the SQL query and load result into a DataFrame
+        df = pd.read_sql(sql, engine)
+        state = df.iloc[0]['motorState']
+        if (state == newState):
+            return newState
+        else:
+            return state
+        
+    except Exception as e:
+        print("Error fetching latest data:", e)
+        return None
